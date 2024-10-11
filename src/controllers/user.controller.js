29588b2402bbf756a7a14c2dd16163ed89dcbb3b
Ali <126307892+alicode43@users.noreply.github.com>
2024-10-11 22:11:20 +0530

    write contollers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,8 @@ import { asyncHander } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHander(async (req, res) => {
 
-
+    const {fullName, email, username,password}=req.body;
+    console.log(fullName, email, username,password );
 
     res.status(200).json({
          message: "Register gi user"
